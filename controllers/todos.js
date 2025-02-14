1e1e821db41b6ad951a68923a79e24912fcfec92
Toomas <toomas.kaarel@gmail.com>
2025-02-14 13:32:26 +0200

    get all todos

---
+++ controllers/todos.js
@@ -0,0 +1,29 @@
+import { Todo } from '../models/todo.js'
+
+class todoController {
+    constructor() {
+        //hold todo projects in array
+        this.TODOS = []
+    }
+
+    createTodo(req,res) {
+        //get data from POST request
+        const task = req.body.task
+        //create new object via Todo model
+        //model constructor uses unique id and task name as parameter
+        const newTodo = new Todo(Math.random().toString(), task)
+        //add new Todo to todos array
+        this.TODOS.push(newTodo)
+        //create a correct response
+        res.json({
+            message: 'created new todo object',
+            newTask: newTodo
+        })
+    }
+
+    getTodos(req, res) {
+        res.json({tasks: this.TODOS})
+    }
+}
+
+export const TodoController = new todoController()
