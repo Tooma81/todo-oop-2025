cc4ac8a8f8540732f17b6f5037d521706f948a28
Toomas <toomas.kaarel@gmail.com>
2025-02-14 15:38:26 +0200

    write and read todos from file

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,12 +1,13 @@
 import { Todo } from '../models/todo.js'
+import { fileManager } from '../utils/files.js'
 
 class todoController {
     constructor() {
-        //hold todo projects in array
-        this.TODOS = []
+        //try to get data from file and init tasks array
+        this.initTodos()
     }
 
-    createTodo(req,res) {
+    async createTodo(req,res) {
         //get data from POST request
         const task = req.body.task
         //create new object via Todo model
@@ -14,6 +15,8 @@ class todoController {
         const newTodo = new Todo(Math.random().toString(), task)
         //add new Todo to todos array
         this.TODOS.push(newTodo)
+        //save data to file
+        await fileManager.writeFile('./data/todos.json', this.TODOS)
         //create a correct response
         res.json({
             message: 'created new todo object',
@@ -21,11 +24,21 @@ class todoController {
         })
     }
 
+    async initTodos() {
+        const todosData = await fileManager.readFile('./data/todos.json')
+        //if file is ok, add file data to array
+        if(todosData !== null) {
+            this.TODOS = todosData
+        } else {
+            this.TODOS = []
+        }
+    }
+
     getTodos(req, res) {
         res.json({tasks: this.TODOS})
     }
 
-    updateTodo(req,res) {
+    updateTodo(req, res) {
         const todoId = req.params.id
         const updatedTask = req.body.task
 
