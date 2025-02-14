e0a2caa1395f0020277dc367a5ed364f69718bf2
Toomas <toomas.kaarel@gmail.com>
2025-02-14 14:27:30 +0200

    delete todo by index

--- controllers/todos.js
+++ controllers/todos.js
@@ -47,6 +47,28 @@ class todoController {
             updatedTask: this.TODOS[todoIndex]
         })
     }
+
+    deleteTodo(req,res) {
+        const todoId = req.params.id
+        const updatedTask = req.body.task
+
+        console.log(req.body)
+        console.log(req.params)
+
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+
+        if(todoIndex < 0) {
+            res.json({
+                message: 'Could not find such index'
+            })
+            throw new Error('Could not find todo')
+        }
+
+        this.TODOS.splice(todoIndex, 1)
+        res.json({
+            message: 'todo is deleted'
+        })
+    }
 }
 
 export const TodoController = new todoController()
